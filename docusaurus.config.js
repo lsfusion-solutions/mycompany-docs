73d4df1e114c4cf54a06cfed9876e49c2e3a07d1
Aliaksei <danchenko@gmail.com>
2022-03-24 11:05:22 +0400

    Google analytics

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -38,6 +38,9 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
+        googleAnalytics: {
+          trackingID: 'UA-38678623-1',
+        },
       }),
     ],
   ],
